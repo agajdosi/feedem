49fe4dc623778a0ea6b314af91d46fddd3c1806d
Andreas Gajdosik <andreas@gajdosik.org>
2025-04-17 23:36:15 +0200

    Add some filtering getFunctions into game.service

--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { User, Post, Game } from '../../models/game';
+import { User, Post, Game , Reaction} from '../../models/game';
 // http client
 import { HttpClient } from '@angular/common/http';
 // rxjs
@@ -35,7 +35,7 @@ export class GameService {
 
   // MARK: USER
 
-  getHero(): User {
+  getHero(): User | undefined {
     return this.game.users.find(user => this.game.hero === user.uuid)!;
   }
 
@@ -45,7 +45,35 @@ export class GameService {
   
   // MARK: POSTS
 
-  getPostsByUser(userId: string): Post[] {
+  /** Get all posts by a author-user. */
+  getPostsByAuthor(userId: string): Post[] {
     return this.game.posts.filter(post => post.author === userId);
+  }
+
+  /** Get posts by another user seen by this user. Basically what this user recalls about another user's post activity.
+   * @param userID - uuid of the user who saw something, the recalling user
+   * @param authorID - uuid of the user who wrote the posts
+   * @returns all posts by the other user, author of the posts
+   */
+  getPostsByAuthorSeenByUser(userID: string, authorID: string): Post[] {
+    const viewsByUser = this.game.views.filter(view => view.user === userID);
+    const postsByAuthor = this.getPostsByAuthor(authorID);
+    const posts = viewsByUser.map(view => postsByAuthor.find(post => post.uuid === view.post));
+    return posts.filter(post => post !== undefined) as Post[];
+  }
+
+  /** Get all posts by a user. */
+  getRecentlyViewedPosts(userId: string): Post[] {
+    const views = this.game.views.filter(view => view.user === userId);
+    const posts = views.map(view => this.game.posts.find(post => post.uuid === view.post));
+    return posts.filter(post => post !== undefined) as Post[];
+  }
+
+  // MARK: REACTIONS
+
+  /** Get all reactions by a author-user. */
+  getReactionsByUser(authorID: string): Reaction[] {
+    const reactions = this.game.reactions.filter(reaction => reaction.author === authorID);
+    return reactions;
   }
 }
