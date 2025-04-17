a81da910071e45bb7a60b00c99365790e31090ea
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-17 10:28:18 +0200

    move initial-game load to game.service

--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@angular/core';
 import { User } from '../../models/game';
 // models
 import { Game } from '../../models/game';
+// http client
+import { HttpClient } from '@angular/common/http';
 // rxjs
 import { Subject } from 'rxjs';
 
@@ -11,17 +13,29 @@ import { Subject } from 'rxjs';
 
 export class GameService {
   game: Subject<Game> = new Subject();
+  private _game!: Game;
   // TODO -> USERS TO USERS.SERVICE
   private users: User[] = [];
 
-
-
-  constructor() {
+  constructor(
+    private readonly http: HttpClient
+  ) {
     this.game.subscribe({
       next: (game: Game) => {
+        if (!this._game) this._game = game;
         if (game.users) this.users = game.users;
       }
-    })
+    });
+    // get initial game json
+    const initialGameSub = this.http.get<Game>('/initial-game.json').subscribe({
+      next: (game: Game) => {
+        // console.log('initial game', game);
+        if (!this._game && game && game.uuid) {
+          this.game.next(game);
+          initialGameSub.unsubscribe();
+        }
+      }
+    });
   }
 
   getUsers(): User[] {
