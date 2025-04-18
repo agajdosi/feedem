965f7d95dd999f4775088df3c9a76f7e975a7c01
Andreas Gajdosik <andreas@gajdosik.org>
2025-04-18 14:02:54 +0200

    Move util functuions from game to utils.ts - transform them into pure functions - add tests for those pure functions

--- src/app/services/game/game.service.ts
+++ src/app/services/game/game.service.ts
@@ -78,38 +78,4 @@ export class GameService {
   getUserById(userId: string): User | undefined {
     return this.game.users.find(user => user.uuid === userId);
   }
-  
-  // MARK: POSTS
-
-  /** Get all posts by a author-user. */
-  getPostsByAuthor(userId: string): Post[] {
-    return this.game.posts.filter(post => post.author === userId);
-  }
-
-  /** Get posts by another user seen by this user. Basically what this user recalls about another user's post activity.
-   * @param userID - uuid of the user who saw something, the recalling user
-   * @param authorID - uuid of the user who wrote the posts
-   * @returns all posts by the other user, author of the posts
-   */
-  getPostsByAuthorSeenByUser(userID: string, authorID: string): Post[] {
-    const viewsByUser = this.game.views.filter(view => view.user === userID);
-    const postsByAuthor = this.getPostsByAuthor(authorID);
-    const posts = viewsByUser.map(view => postsByAuthor.find(post => post.uuid === view.post));
-    return posts.filter(post => post !== undefined) as Post[];
-  }
-
-  /** Get all posts by a user. */
-  getRecentlyViewedPosts(userId: string): Post[] {
-    const views = this.game.views.filter(view => view.user === userId);
-    const posts = views.map(view => this.game.posts.find(post => post.uuid === view.post));
-    return posts.filter(post => post !== undefined) as Post[];
-  }
-
-  // MARK: REACTIONS
-
-  /** Get all reactions by a author-user. */
-  getReactionsByUser(authorID: string): Reaction[] {
-    const reactions = this.game.reactions.filter(reaction => reaction.author === authorID);
-    return reactions;
-  }
 }
--- src/app/shared/utils.ts
+++ src/app/shared/utils.ts
@@ -1,3 +1,6 @@
+import { Post, User, View, Reaction, Comment } from "../models/game";
+
+
 /**Get the required limit of the users' engagement. 
  * If Average Engagement is lower than the limit, Game is over. Sorry, not sorry.
  */
@@ -12,4 +15,59 @@ export function getLimit(tasks: number): number {
 export function  getAvgEngagement(views: number, comments: number, reactions: number): number {
   if (views === 0) return 200;
   return 100 * (reactions + comments) / views;
+}
+
+
+// MARK: POSTS
+
+export function getPostsByAuthor(author: User, allPosts: Post[]): Post[] {
+  return allPosts.filter(post => post.author === author.uuid);
+}
+
+/** Get posts by another user seen by this user. Basically what this user recalls about another user's post activity.
+ * @param user - uuid of the user who saw something, the recalling user
+ * @param author - uuid of the user who wrote the posts
+ * @returns all posts by the author
+ */
+export function getPostsByAuthorSeenByUser(user: User, author: User, views: View[], posts: Post[]): Post[] {
+  const viewsByUser = views.filter(view => view.user === user.uuid);
+  const postsByAuthor = getPostsByAuthor(author, posts);
+  const seenPosts = viewsByUser.map(view => postsByAuthor.find(post => post.uuid === view.post));
+  return seenPosts.filter(post => post !== undefined) as Post[];
+}
+
+
+/** From input posts, get just the ones where user has interacted (comment or reaction). */
+export function getPostsWhereUserInteracted(user: User, comments: Comment[], reactions: Reaction[], posts: Post[]): Post[] {
+  const userComments = comments.filter(comment => comment.author === user.uuid);
+  const userReactions = reactions.filter(reaction => reaction.author === user.uuid);
+  const postsWithInteractions = [...userComments, ...userReactions].map(interaction => interaction.parent);
+  return posts.filter(post => postsWithInteractions.includes(post.uuid));
+}
+
+
+/** Return just the posts that the user has seen. Simulates the user's memory. */
+export function filterSeenPosts(user: User, views: View[], posts: Post[]): Post[] {
+  return posts.filter(post => views.some(view => view.post === post.uuid && view.user === user.uuid));
+}
+
+
+// MARK: COMMENTS
+
+export function getCommentsByUser(comments: Comment[], user: User): Comment[] {
+  return comments.filter(comment => comment.author === user.uuid);
+}
+export function getCommentsUnderPost(comments: Comment[], post: Post): Comment[] {
+  return comments.filter(comment => comment.parent === post.uuid);
+}
+
+
+
+// MARK: REACTIONS
+
+export function getReactionsByUser(reactions: Reaction[], user: User): Reaction[] {
+  return reactions.filter(reaction => reaction.author === user.uuid);
+}
+export function getReactionsUnderPost(reactions: Reaction[], post: Post): Reaction[] {
+  return reactions.filter(reaction => reaction.parent === post.uuid);
 }
