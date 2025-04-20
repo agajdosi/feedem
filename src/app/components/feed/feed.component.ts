a18ba5768b40622a8a7ec96cf9ffea39d03347b3
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-20 22:17:33 +0200

    show tasks posts

--- src/app/components/feed/feed.component.ts
+++ src/app/components/feed/feed.component.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+// interfaces
+import { Game, Post } from '../../models/game';
+// components
+import { PostComponent } from '../post/post.component';
+// services
+import { GameService } from '../../services/game/game.service';
+// rxjs
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-feed',
-  imports: [],
+  imports: [ PostComponent ],
   templateUrl: './feed.component.html',
   styleUrl: './feed.component.scss'
 })
-export class FeedComponent {
+export class FeedComponent implements OnInit, OnDestroy {
 
+  game!: Game;
+  private gameSub: Subscription = new Subscription();
+
+  constructor(
+    private readonly gameService: GameService
+  ){}
+
+  ngOnInit(): void {
+    this.gameSub = this.gameService.gameSubject.subscribe({
+      next: (game: Game) => this.game = game
+    });
+  }
+
+  getPost(id: string): Post {
+    return this.gameService.getPost(id);
+  }
+
+  ngOnDestroy(): void {
+    this.gameSub.unsubscribe();
+  }
 }
