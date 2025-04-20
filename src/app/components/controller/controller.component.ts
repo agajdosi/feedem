e586a3fc75da047ed86286ebb3eb4296a3333eb0
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-20 22:17:52 +0200

    💥

--- src/app/components/controller/controller.component.ts
+++ src/app/components/controller/controller.component.ts
@@ -1,18 +1,21 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
+// router
+import { ActivatedRoute } from '@angular/router';
 // models
 import { Game } from '../../models/game';
 // components
 import { UserComponent } from '../user/user.component';
 import { OnScreenComponent } from '../on-screen/on-screen.component';
+import { TaskComponent } from '../task/task.component';
 // services
-import { SocketEvent, SocketService } from '../../services/socket/socket.service';
+import { SocketEvent, SocketService, SocketCommand } from '../../services/socket/socket.service';
 import { GameService } from '../../services/game/game.service';
 // rxjs
 import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-controller',
-  imports: [ UserComponent, OnScreenComponent ],
+  imports: [ UserComponent, OnScreenComponent, TaskComponent ],
   templateUrl: './controller.component.html',
   styleUrl: './controller.component.scss'
 })
@@ -25,13 +28,17 @@ export class ControllerComponent implements OnInit, OnDestroy {
   controllable: boolean = false;
   game!: Game;
   highlightedUser: string = '';
+  connectedGameInstance: string | null = '';
 
   constructor(
     private readonly socketService: SocketService,
-    private readonly gameService: GameService
+    private readonly gameService: GameService,
+    private readonly route: ActivatedRoute
   ){}
 
   ngOnInit(): void {
+    // TODO: if connectedGameInstance disconnect, remove control from this controller (server side)
+    this.connectedGameInstance = this.route.snapshot.queryParams['from'];
     /* 
       1. wait for game
       2. request game control
@@ -66,6 +73,18 @@ export class ControllerComponent implements OnInit, OnDestroy {
     this.requestGameControl();
   }
 
+  selectHero(userId: string): void {
+    console.log('select HERO', userId);
+    this.gameService.setHero(userId);
+    this.socketService.sendSocketMessage({
+      command: 'select-hero',
+      data: {
+        userId: userId
+      }
+    });
+    this.gameService.nextTask();
+  }
+
   ngOnDestroy(): void {
     this.gameControlSub.unsubscribe();
     this.gameSub.unsubscribe();
@@ -76,10 +95,21 @@ export class ControllerComponent implements OnInit, OnDestroy {
     console.log(`user ${id} is on screen...`);
     // send socket message to highlight on graph
     this.highlightedUser = id;
-    this.socketService.sendSocketMessage({
-      command: "highlight-user-on-graph",
-      userId: id
-    });
+    const socketCommand: SocketCommand = {
+      command: 'highlight-user-on-graph',
+      data: {
+        userId: id
+      }
+    }
+    this.socketService.sendSocketMessage(socketCommand);
+  }
+
+  isAnyOpenTask(): boolean {
+    if (!this.game.tasks.length) return false;
+    for (const task of this.game.tasks) {
+      if (!task.completed) return true;
+    }
+    return false;
   }
 
   private requestGameControl(): void {
--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 // components
 import { UserComponent } from '../user/user.component';
 import { FeedComponent } from '../feed/feed.component';
@@ -7,8 +7,8 @@ import { Game, User } from '../../models/game';
 import { GraphNode } from '../../models/graph-node';
 import { GraphEdge } from '../../models/graph-edge';
 // services
-import { SocketService, SocketEvent } from '../../services/socket/socket.service';
-import { HttpService } from '../../services/http/http.service';
+import { SocketService, SocketEvent, SocketCommand } from '../../services/socket/socket.service';
+// import { HttpService } from '../../services/http/http.service';
 import { GameService } from '../../services/game/game.service';
 import { GraphService } from '../../services/graph/graph.service';
 // rxjs
@@ -26,6 +26,7 @@ import {
   GraphLayoutSettings,
   GraphData
 } from '@tomaszatoo/graph-viewer';
+import Graph from 'graphology';
 
 @Component({
   selector: 'app-game',
@@ -42,10 +43,16 @@ import {
 })
 export class GameComponent implements OnInit, OnDestroy {
   
+
   controllable: boolean = false;
 
   graphLayoutSettings!: GraphLayoutSettings
   graphData!: GraphData;
+  highlightUser: string | undefined  = undefined;
+  highlightUsersConnection: string | undefined = undefined;
+  clearUsersHighlight: boolean = false;
+  clearConnectionsHighlight: boolean = false;
+  graph!: Graph;
 
   game!: Game;
 
@@ -58,7 +65,7 @@ export class GameComponent implements OnInit, OnDestroy {
 
   constructor(
     private readonly socketService: SocketService,
-    private readonly httpService: HttpService,
+    // private readonly httpService: HttpService,
     private readonly gameService: GameService,
     private readonly llmsService: LlmsService,
     private readonly graphService: GraphService
@@ -67,28 +74,35 @@ export class GameComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit(): void {
+    // window.addEventListener('beforeunload', this.notifyUsersAboutLeaving.bind(this));
+    // this.socketService.sendSocketMessage({
+    //   command: 'deactivate-controller-of',
+    //   data: {
+    //     userId: this.socketService.socketId
+    //   }
+    // });
     // subscribe game json
     this.gameService.gameSubject.subscribe({
       next: (game: Game) => {
         if (game && game.uuid) {
-          if (this.game && game.updated > this.game.updated || !this.game) {
+          if (this.game/*  && game.updated > this.game.updated */) {
+            // TODO: update graph
+            // ... add posts, mainly
+            //
+            console.warn('GAME SHOULD BE JUST UPDATED');
+            this.gameService.updateGame(this.game, game);
+            console.log('updated game', this.game);
+          } else {
             this.game = game;
             // build user graph
             console.log('this.game', this.game);
             this.graphService.buildGraph(this.game.users, this.game.relations, this.game.posts).then((graphData: GraphData | undefined) => {
               if (graphData) this.graphData = graphData;
             }).catch((e: any) => console.error(e));
-          } 
+          }
         }
       }
     })
-    // get initial game json
-    // this.httpService.get<Game>('/initial-game.json').subscribe({
-    //   next: (game: Game) => {
-    //     // console.log('initial game', game);
-    //     if (game && game.uuid) this.gameService.game.next(game);
-    //   }
-    // });
     // listen to all client messages
     this.socketSub = this.socketService.socketMessage.subscribe({
       next: (e: SocketEvent) => this.socketMessageHandler(e)
@@ -106,6 +120,7 @@ export class GameComponent implements OnInit, OnDestroy {
   }
 
   ngOnDestroy(): void {
+    // window.removeEventListener('beforeunload', this.notifyUsersAboutLeaving);
     this.socketSub.unsubscribe();
     this.gameControllableSub.unsubscribe();
     this.gameSub.unsubscribe();
@@ -117,7 +132,8 @@ export class GameComponent implements OnInit, OnDestroy {
 
   getControllerLink(): string {
     const location = window.location;
-    const link = `${location.protocol}//${location.host}/controller`;
+    const mySocketId = this.socketService.socketId;
+    const link = `${location.protocol}//${location.host}/controller?from=${mySocketId}`;
     // console.log(link);
     return link;
   }
@@ -126,6 +142,27 @@ export class GameComponent implements OnInit, OnDestroy {
     const link = this.getControllerLink();
     window.open(link, '_blank');
   }
+
+  graphUpdated(graph: Graph): void {
+    if (!this.graph) console.log('graphInitialised', graph);
+    if (this.graph) console.log('graphUpdated', graph);
+    if (graph) this.graph = graph;
+  }
+
+  // TODO !!! -> ONLY SERVER CAN TRIGGER LEAVING (should know if the controlled game instance stoped)
+  // private notifyUsersAboutLeaving(e: BeforeUnloadEvent): void {
+  //   // send message to deactivate controller of this instance of game being controlled
+  //   e.preventDefault();
+  //   console.log('BEFOREUNLOAD', e);
+  //   this.socketService.sendSocketMessage({
+  //     command: 'deactivate-controller-of',
+  //     data: {
+  //       userId: this.socketService.socketId
+  //     }
+  //   });
+  //   // e.preventDefault();
+  //   // return 'Test';
+  // }
 
   private createNodeRenderer() {
     return ({ attributes, position }: any) => {
@@ -151,21 +188,61 @@ export class GameComponent implements OnInit, OnDestroy {
     switch (e.type) {
       case 'game':
         console.log('game from server', e.data);
-        if (e.data && e.data.uuid) this.gameService.gameSubject.next(e.data);
+        if (e.data && e.data.uuid) this.gameService.gameSubject.next(e.data); // TODO: update game, not replace
+        break;
+      case 'message':
+        this.socketCommandsHandler(e.data);
         break;
       default:
         console.log('recieved socket event', e);
     }
   }
 
-  saveGame(): void {
+  private socketCommandsHandler(c: SocketCommand): void {
+    console.log('socketCommand', c);
+    switch (c.command) {
+      case 'highlight-user-on-graph':
+        // TODO: not a nice solution :/
+        this.clearUsersHighlight = true;
+        this.clearConnectionsHighlight = true;
+        setTimeout(() => {
+          this.highlightUser = c.data.userId;
+          this.clearUsersHighlight = false;
+          this.clearConnectionsHighlight = false;
+          const connections: string[] = [];
+          this.graph.findEdge(c.data.userId, (edge: string, attributes: any) => {
+            // console.log('user connection to highlight', edge);
+            connections.push(edge);
+          });
+          if (connections.length) {
+            let i = 0;
+            const add = setInterval(() => {
+              if (i > connections.length) clearInterval(add);
+              this.highlightUsersConnection = connections[i];
+              i++;
+            }, 10);
+          }
+        }, 100);        
+        break;
+      case 'select-hero':
+        this.gameService.setHero(c.data.userId);
+        break;
+      case 'update-game':
+        this.gameService.gameSubject.next(c.data.game);
+        break;
+      default:
+        console.log('recieved socket command', c);
+    }
+  }
 
+  saveGame(): void {
+    // cannot save game from this point, only controllers can
   }
 
   
 
   
-
+  // CANNOT SEND SOCKET MESSAGE IF IM NOT ON CONTROL (ONLY CONTROLLER)
   private sendSocketMessage(message: any): void {
     this.socketService.sendSocketMessage(message);
   }
