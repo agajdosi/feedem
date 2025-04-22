dfbd2fdfc2f2cc11008932bc5547875fe53ab8a0
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-22 10:47:49 +0200

    add comments / reactions + refactor

--- src/app/components/post/post.component.ts
+++ src/app/components/post/post.component.ts
@@ -1,13 +1,15 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+// components
+import { CommentComponent } from '../comment/comment.component';
 // interfaces
-import { Post, User } from '../../models/game';
+import { Post, User, Game, Reaction, Comment } from '../../models/game';
 // services
 import { GameService } from '../../services/game/game.service';
 
 @Component({
   selector: 'app-post',
-  imports: [ CommonModule ],
+  imports: [ CommonModule, CommentComponent ],
   templateUrl: './post.component.html',
   styleUrl: './post.component.scss'
 })
@@ -15,15 +17,19 @@ export class PostComponent implements OnInit {
 
   @Input() post!: Post;
 
+  get game(): Game {
+    return this.gameService.game;
+  }
+
+  get author(): User {
+    return this.gameService.getUserById(this.post.author);
+  }
+
   constructor(
     private readonly gameService: GameService
   ){}
 
   ngOnInit(): void {
     
-  }
-
-  getPostAuthor(): User {
-    return this.gameService.getUserById(this.post.author);
   }
 }
