3a88d292f9c25d2e1f3d64c2644bb68799e59a22
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-22 21:26:10 +0200

    ...

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, OnDestroy, HostListener } from '@angular/core';
 import { UserComponent } from '../user/user.component';
 import { FeedComponent } from '../feed/feed.component';
 // models
-import { Game, User } from '../../models/game';
+import { Game, User, Relation, RelationType } from '../../models/game';
 import { GraphNode } from '../../models/graph-node';
 import { GraphEdge } from '../../models/graph-edge';
 // services
@@ -108,29 +108,56 @@ export class GameComponent implements OnInit, OnDestroy {
                   console.log('NEW TASK', t);
                   // 
                   const post = game.posts.filter(post => post.uuid === t.showPost)[0];
-                  const autor = post.author;
-                  const node = {};
-                  (node as any)[post.uuid] = {type: 'post', x: 1, y: 1};
-                  this.addGraphNodes = node;
-                  // this.graph.addNode(post.uuid, {type: 'post', x: 1, y: 1});
+                  const author = post && post.author ? post.author : null;
+                  if (post && author) {
+                    const node = {};
+                    (node as any)[post.uuid] = {type: 'post', x: 1, y: 1};
+                    this.addGraphNodes = node;
+                    // this.graph.addNode(post.uuid, {type: 'post', x: 1, y: 1});
 
-                  if (t.type === 'showPost' && t.showTo.length) {
-                    const showTo = t.showTo[0];
-                    this.addGraphEdges = [
-                      {source: autor, target: post.uuid, attributes: {label: 'sent'}},
-                      {source: post.uuid, target: showTo, attributes: {label: 'got'}}
-                    ];
+                    if (t.type === 'showPost' && t.showTo.length) {
+                      const showTo = t.showTo[0];
+                      this.addGraphEdges = [
+                        {source: author, target: post.uuid, attributes: {label: 'sent'}},
+                        {source: post.uuid, target: showTo, attributes: {label: 'got'}}
+                      ];
+                    }
+                    if (t.type === 'distributePost') {
+                      console.warn('TODO');
+                      // for (const showTo of t.showTo) {
+                      //   const tmp: {source: string, target: string, attributes: any}[] = [];
+                      //   let edgesSolved = 0;
+                      //   this.graph.findEdge(author, post.uuid, (edge: string) => {
+                      //     if (edge) {
+                      //       console.log('edge already exist, add label', edge);
+                      //       this.graph.setEdgeAttribute(edge, 'label', `${this.graph.getEdgeAttribute(edge, 'label')}, ${RelationType.Write}`);
+                      //     } else {
+                      //       tmp.push({source: author, target: post.uuid, attributes: { label: RelationType.Write}});
+                      //       // const relation: Relation = {
+                      //       //   source: author,
+                      //       //   target: post.uuid,
+                      //       //   label: RelationType.Write
+                      //       // }
+                      //       // this.game.relations.push(relation);
+                      //     }
+                      //     edgesSolved++;
+                      //     if (edgesSolved === 2) {
+                      //       this.addGraphEdges = tmp;
+                      //     }
+                      //   });
+                      //   this.graph.findEdge(post.uuid, showTo, (edge: string) => {
+                      //     if (edge) {
+                      //       this.graph.setEdgeAttribute(edge, 'label', `${this.graph.getEdgeAttribute(edge, 'label')}, ${RelationType.Get}`);
+                      //     } else {
+                      //       tmp.push({source: post.uuid, target: showTo, attributes: {label: RelationType.Get}});
+                      //       
+                      //     }
+                      //   })
+                      // }
+                      
+                    } 
                   }
-                  if (t.type === 'distributePost') {
-                    // const tmp = [];
-                    // for (const showTo of t.showTo) {
-                    //   tmp.push(
-                    //     {source: autor, target: post.uuid, attributes: {label: 'sent'}},
-                    //     {source: post.uuid, target: showTo, attributes: {label: 'got'}}
-                    //   );                      
-                    // }
-                    // this.addGraphEdges = tmp;
-                  }                  
+                                   
                 }
                 // console.log('POST TO ADD', post);
                 // const record = {};
@@ -148,7 +175,19 @@ export class GameComponent implements OnInit, OnDestroy {
             // build user graph
             console.log('this.game', this.game);
             this.graphService.buildGraph(this.game.users, this.game.relations, this.game.posts).then((graphData: GraphData | undefined) => {
-              if (graphData) this.graphData = graphData;
+              if (graphData) {
+                this.graphData = graphData;
+                // temp – create realtions
+                // for (const edge of graphData.edges) {
+                //   const relation: Relation = {
+                //     source: edge.source,
+                //     target: edge.target,
+                //     label: RelationType.Follow
+                //   }
+                //   this.game.relations.push(relation);
+                // }
+                console.log('RELATIONS', this.game.relations);
+              }
             }).catch((e: any) => console.error(e));
           }
         }
@@ -267,18 +306,36 @@ export class GameComponent implements OnInit, OnDestroy {
           }
         }, 100);        
         break;
-      case 'highlight-graph-edges':
-        const edges = c.data.edges;
-        console.log('highlight-graph-edges', edges);
+      case 'highlight-graph-path':
+        const path = c.data.path;
+        console.log('TODO: highlight-graph-path', path);
+        console.warn('debug');
+        this.clearConnectionsHighlight = true;
+        this.highlightUsersConnection = undefined;
         let i = 0;
-        const add = setInterval(() => {
-          if (i > edges.length) clearInterval(add);
-          this.highlightUsersConnection = edges[i];
-          i++;
-        }, 10);
+        const highlightEdge = () => {
+          const source = path[i];
+          const target = path[i+1];
+          if (target && source) {
+            this.highlightUsersConnection = undefined;
+            // console.log('source----> surname: ', this.gameService.getUserById(source).surname);
+            // console.log('target----> surname: ', this.gameService.getUserById(target).surname);
+            this.graph.findEdge(source, target, (edge: string) => {
+              // console.log('FOUND EDGE', edge);
+              this.highlightUsersConnection = edge;
+              i++;
+              setTimeout(() => highlightEdge(), 10);
+            });
+          } else {
+            this.clearConnectionsHighlight = false;
+          }
+          
+        }
+        setTimeout(() => highlightEdge(), 10);
         break;
       case 'select-hero':
         this.gameService.setHero(c.data.userId);
+        console.log('SELECT HERO', c.data.userId);
         this.selectUser = c.data.userId;
         break;
       case 'update-game':
