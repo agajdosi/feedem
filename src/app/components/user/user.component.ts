13be9926f8f809b019cb69bf8e39191661f7bde1
Tomáš Javůrek <xvjavurek@vutbr.cz>
2025-04-22 19:43:45 +0200

    add radar + restyle

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,13 +1,39 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 // models
-import { User } from '../../models/game';
+import { BigFive, User } from '../../models/game';
+// components
+import { RadarComponent, RadarData, RadarDataItem } from '../radar/radar.component';
+// uuid
+import { v4 as uuidv4 } from 'uuid';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [ RadarComponent ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent {
+export class UserComponent implements OnInit {
   @Input() user!: User | undefined;
+
+  radarData: RadarData[] = [];
+  constructor() {}
+
+  ngOnInit(): void {
+    if (this.user && this.user.big_five) {
+      const data: RadarData = {
+        id: uuidv4(),
+        items: [],
+        name: '',
+        color: '#ddd'
+      };
+      for (const key in this.user.big_five) {
+        data.items.push({
+          name: key,
+          value: this.user.big_five[key as keyof BigFive],
+          color: '#ddd'
+        });
+      }
+      this.radarData.push(data);
+    }
+  }
 }
