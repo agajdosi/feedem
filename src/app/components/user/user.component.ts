7d6b48cd83bbdf39ad6919e8b063326736b587df
Andreas Gajdosik <andreas@gajdosik.org>
2025-04-23 13:09:44 +0200

    Load/Type the bio in user.component

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,10 +1,12 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnInit, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 // models
 import { BigFive, User } from '../../models/game';
 // components
 import { RadarComponent, RadarData, RadarDataItem } from '../radar/radar.component';
 // uuid
 import { v4 as uuidv4 } from 'uuid';
+import Typed from 'typed.js';
+import { GameService } from '../../services/game/game.service';
 
 @Component({
   selector: 'app-user',
@@ -12,11 +14,14 @@ import { v4 as uuidv4 } from 'uuid';
   templateUrl: './user.component.html',
   styleUrl: './user.component.scss'
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, AfterViewInit {
   @Input() user!: User | undefined;
+  @ViewChild('bioElement') bioElement!: ElementRef;
 
   radarData: RadarData[] = [];
-  constructor() {}
+  isHero: boolean = false;
+
+  constructor(private gameService: GameService) {}
 
   ngOnInit(): void {
     if (this.user && this.user.big_five) {
@@ -34,6 +39,22 @@ export class UserComponent implements OnInit {
         });
       }
       this.radarData.push(data);
+    }
+    this.isHero = this.user?.uuid === this.gameService.game.hero;
+  }
+
+  ngAfterViewInit() {
+    if (this.user?.bio) {
+      const bioTyped = new Typed(this.bioElement.nativeElement, {
+        strings: [this.user.bio],
+        typeSpeed: 5,
+        showCursor: false,
+        loop: true,
+        backDelay: 60000, // 60 seconds
+        backSpeed: 1,
+        fadeOut: true,
+        // fadeOutClass: 'typed-fade-out',
+      });
     }
   }
 }
